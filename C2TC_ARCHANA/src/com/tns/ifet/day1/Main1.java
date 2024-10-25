1c365883c2ed6f386c785ba69f7ede21ce2a9344
archa <archa@Archanapriya>
2024-10-25 15:43:35 +0530

    commit

---
+++ C2TC_ARCHANA/src/com/tns/ifet/day1/Main1.java
@@ -0,0 +1,14 @@
+package com.tns.ifet.day1;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		int $number = 20; //starts with $
+		System.out.println("value of the number variable is : "+ $number);
+		String studentName="Anike";
+		System.out.println("value is : "+ 
+		studentName);
+
+	}
+
+}
