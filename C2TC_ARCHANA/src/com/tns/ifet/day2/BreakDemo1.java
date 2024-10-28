b0b5d484aa66f024aeb23c7483dd85a272587b66
archa <archa@Archanapriya>
2024-10-28 09:47:10 +0530

    commit

---
+++ C2TC_ARCHANA/src/com/tns/ifet/day2/BreakDemo1.java
@@ -0,0 +1,14 @@
+package com.tns.ifet.day2;
+
+public class BreakDemo1 {
+
+	public static void main(String[] args) {
+		for (int i = 5; i < 10; i++) {
+			if (i == 5)
+			break;
+			System.out.println(i);
+
+	}
+
+}
+}
