13d1f4d3feffe6a1eb7aa62aaa37300c674457d3
archa <archa@Archanapriya>
2024-11-06 15:24:25 +0530

    commit

---
+++ C2TC_ARCHANA/src/com/tns/ifet/day4/Shape.java
@@ -0,0 +1,11 @@
+package com.tns.ifet.day4;
+public abstract class Shape {
+	protected float area;
+    
+	 abstract void calArea();
+	 
+	 void show()
+	 {
+		 System.out.println("Area of shape is "+area);
+	 }
+}
---
+++ C2TC_ARCHANA/src/com/tns/ifet/day4/Square.java
@@ -0,0 +1,16 @@
+package com.tns.ifet.day4;
+public class Square  extends Shape{
+	private float side;
+	public Square()
+	{
+		side=2.0f;
+	}
+	public Square(float side)
+	{
+		this.side=side;
+	}
+	
+	void calArea() {
+		super.area=side*side;		
+	}
+}
